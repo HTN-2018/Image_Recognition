eaf84f5ad44892f20cf57eb4ca37bfb33074649b
MxMoss3 <mossberg.max@gmail.com>
2018-09-15 10:56:41 -0400

    cleaned up code, added multiple face detection

--- door_monitor.py
+++ door_monitor.py
@@ -4,7 +4,7 @@ import time
 import json
 from watson_developer_cloud import VisualRecognitionV3
 
-#Initialization
+#Initializastion
 visual_recognition = VisualRecognitionV3(
     version='2018-03-19',
     iam_apikey='vjmKV_6yhL6kBnWZ5rMsHNRgLStIM7LkE-m3pybypuUw'
@@ -12,62 +12,52 @@ visual_recognition = VisualRecognitionV3(
 
 last_frame = 0
 
-cap = cv2.VideoCapture(0)
-while(cap.isOpened()):
-    
-    #captures frame from video feed
-    ret, img = cap.read()
-    cv2.imshow("Door Feed", img)
+def save_faces(faces, img):
 
-    
-    if time.time() - last_frame > 1:
-        last_frame = time.time()
+    print("A PERSON!!! \(OoO)/")
+    print(len(faces['images'][0]['faces']))
+    for i in range(len(faces['images'][0]['faces'])): 
+        x = faces['images'][0]['faces'][i]['face_location']['left']
+        y = faces['images'][0]['faces'][i]['face_location']['top']
+        dx = faces['images'][0]['faces'][i]['face_location']['width']
+        dy = faces['images'][0]['faces'][i]['face_location']['height']
 
-        cv2.imwrite("test.jpg", img)
-
-        with open('./test.jpg', 'rb') as images_file:
-            faces = visual_recognition.detect_faces(images_file).get_result()
-        #print(json.dumps(faces, indent=2))
-
-        if len(faces['images'][0]['faces']) > 0:
-            print("A PERSON!!! \(OoO)/")
-
-            x = faces['images'][0]['faces'][0]['face_location']['left']
-            y = faces['images'][0]['faces'][0]['face_location']['top']
-            dx = faces['images'][0]['faces'][0]['face_location']['width']
-            dy = faces['images'][0]['faces'][0]['face_location']['height']
-
-            #face_crop = img[x:x+dx, y:y+dy]
-            face_crop = img[y:y+dy, x:x+dx]
-            cv2.imshow("face_crop", face_crop)
-            cv2.imwrite("face_crop.jpg", face_crop)
-
-            
-            # for detecting multiple faces
-            '''
-            for i in range(len(faces['images'][0]['faces'])): 
-                x = faces['images'][0]['faces'][i]['face_location']['left']
-                y = faces['images'][0]['faces'][i]['face_location']['top']
-                dx = faces['images'][0]['faces'][i]['face_location']['width']
-                dy = faces['images'][0]['faces'][i]['face_location']['height']
-
-                face_crop = img[x:y, dx:dy]
-                cv2.imwrite("face"+str(i)+".jpg", img)
-
-            #send picture of person's face to firebase
-            #send video of their actions to firebase
-            '''
+        face_crop = img[y:y+dy, x:x+dx]
+        cv2.imwrite("face"+str(i)+".jpg", face_crop)
+        cv2.imshow("face"+str(i)+".jpg", face_crop)
 
 
-        else:
-            print("no person -.-")
-            
+def face_search(img):
+    cv2.imwrite("test.jpg", img)
 
+    with open('./test.jpg', 'rb') as images_file:
+        faces = visual_recognition.detect_faces(images_file).get_result()
+    #print(json.dumps(faces, indent=2))
+
+    if len(faces['images'][0]['faces']) > 0:
+        save_faces(faces, img)
+    else:
+        print("no person -.-")
+
+
+def main():
+    cap = cv2.VideoCapture(1)
+    while(cap.isOpened()):
         
-    #hold escape to exit program
-    k = cv2.waitKey(10)
-    if k == 27:
-        cap.release()
-        break
+        #captures frame from video feed
+        ret, img = cap.read()
+        
+        #checks for faces every second
+        if time.time() - last_frame > 1:
+            last_frame = time.time()
 
+            face_search(img)
+                
+        #hold escape to exit program
+        k = cv2.waitKey(10)
+        if k == 27:
+            cap.release()
+            break
 
+if __name__ == "__main__":
+    main()
