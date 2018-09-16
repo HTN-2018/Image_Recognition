77932ae3f6fbfb5d3c0189d826bd66e324c598c4
MxMoss3 <mossberg.max@gmail.com>
2018-09-16 04:00:42 -0400

    made meta data easier to handle in firebase

--- door_monitor.py
+++ door_monitor.py
@@ -1,5 +1,9 @@
 import cv2
+
 import time
+from datetime import datetime
+import calendar
+import math
 
 import json
 from watson_developer_cloud import VisualRecognitionV3
@@ -9,15 +13,12 @@ import time
 from configuration import *
 from helpers import *
 
-
-#def initialize():
-
+#initialize
 visual_recognition = VisualRecognitionV3(
     version='2018-03-19',
     #iam_apikey='3JFeQ9NxcJjESkniWwfQzMaKin3ff2hbmzfpze7YkdrT'
-    iam_apikey='v7YXRuo_CXPRmOz3QmpbqPQlSfuFHkEKG43YB5urqyJV'
+    iam_apikey='Pk09CTZsWBG3FKzad6fCzGAKzUVFl9hFQDFLZUqO9C19'
 )
-
 
 #set up firebase
 fb = pyrebase.initialize_app(FIREBASE_CONFIG)
@@ -27,13 +28,15 @@ storage = fb.storage()
 
 user = authenticate_user(auth)
 
-
 #print(user)
 
 def log_faces(faces, img):
 
-    print("A PERSON!!! \(OoO)/")
     ppl_num = len(faces['images'][0]['faces'])
+    if ppl_num == 1:
+        print("A PERSON!!! \(OoO)/")
+    elif ppl_num > 1:
+        print( ppl_num, "people! \(OoO)/" )
 
     for i in range(ppl_num): 
         x = faces['images'][0]['faces'][i]['face_location']['left']
@@ -41,24 +44,73 @@ def log_faces(faces, img):
         dx = faces['images'][0]['faces'][i]['face_location']['width']
         dy = faces['images'][0]['faces'][i]['face_location']['height']
 
-        face_crop = img[y:y+dy, x:x+dx]
+        '''
+        #makes sure pictures are square
+        delta = math.floor(abs(dx - dy))
+        if dx > dy:
+            if y - math.floor(.5*delta) <= 0:
+                y = 0
+            elif y + dy + (.5*delta) > max_y:
+                y = max_y - delta
+            else:
+                y -= math.floor(.5*delta)
+            dy += delta
+
+        if dx > dx:
+            if x - math.floor(.5*delta) <= 0:
+                x = 0
+            elif x + dx + (.5*delta) > max_x:
+                x = max_x - delta
+            else:
+                x -= math.floor(.5*delta)
+            dx += delta
+        '''
+
+        face_crop = resize(img[y:y+dy, x:x+dx])
         cv2.imwrite("face"+str(i)+".jpg", face_crop)
         cv2.imshow("face"+str(i)+".jpg", face_crop)
 
     return ppl_num
 
-def send_push(user, faces):
+def resize(img):
+    ratio = 200 / img.shape[0]
+    width = int(img.shape[1] * ratio) 
+    height = int(img.shape[0] * ratio) 
+    
+    dim = (width, height)
 
-    metadata_store = db.child("users").child(user['localId']).push(faces, user['idToken'])
+    resized = cv2.resize(img, dim)
+
+    return resized
+
+def parser(face):
+    age_range = str(face['age']['min']) + "-" + str(face['age']['max']) 
+    gender = face['gender']['gender']
+    date = calendar.month_name[datetime.now().month] + ' ' + str(datetime.now().day)
+    time = str(datetime.now().hour) + ':' + str(datetime.now().minute)
+
+    data = {"age-range" : age_range,
+            "gender" : gender,
+            "time" : date + ', ' + time
+            }
+    return data
+
+def send_push(user, faces):
+    #x = faces['images'][0]['faces'][i]['face_location']['left']
+
+    for i in faces['images'][0]['faces']:
+        face_data = parser(i)
+        metadata_store = db.child("users").child(user['localId']).push(face_data, user['idToken'])
 
 def upload_pics(ppl_num):
     for i in range(ppl_num):
-        media_store = storage.child('images/' + user['localId'] + '/' + str(time.time()) + '.jpg').put("face"+str(i)+".jpg", user['idToken'])
+        media_store = storage.child('images/' + user['localId'] + '/' +
+                str(time.time()) + '.jpg').put("face"+str(i)+".jpg", user['idToken'])
 
 def face_search(img):
-    cv2.imwrite("test.jpg", img)
+    cv2.imwrite("0.jpg", img)
 
-    with open('./test.jpg', 'rb') as images_file:
+    with open('./0.jpg', 'rb') as images_file:
         faces = visual_recognition.detect_faces(images_file).get_result()
 
     if len(faces['images'][0]['faces']) > 0:
@@ -69,21 +121,18 @@ def face_search(img):
     else:
         print("no person -.-")
 
-
 def main():
-    #user, visual_recongnition = initialize()
-
     last_frame = 0
 
-    #for i in range(2):
-    #    media_store = storage.child('images/' + user['localId'] + '/' + str(time.time()) + '.png').put("face"+str(i)+".jpg", user['idToken'])
-
-    cap = cv2.VideoCapture(1)
+    #specifies where you are getting your video feed from
+    cap = cv2.VideoCapture(0)
     while(cap.isOpened()):
         
         #captures frame from video feed
         ret, img = cap.read()
-        
+
+        cv2.imshow("face_cam", img)
+
         #checks for faces every second
         if time.time() - last_frame > 5:
             last_frame = time.time()
@@ -98,31 +147,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-'''
-#Get data video/images/metadata/etc from an event
-
-###TEST EVENTS
-event_1 = {
-    'time': time.time(),
-    'metadata': {'type': 'usps delivery', 'gender': 'female'},
-}
-
-event_1_photo = './photos/twitter_banner.png'
-
-event_2 = {
-    'time': time.time(),
-    'metadata': {'type': 'metronet salesperson', 'gender': 'male'},
-}
-
-event_2_photo = './photos/amazon.png'
-
-# Pass the user's idToken to the push method
-metadata_store = db.child("users").child(user['localId']).push(event_1, user['idToken'])
-
-#Store the image
-#def media_upload(localId, photo, idToken):
-media_store = storage.child('images/' + user['localId'] + '/' + str(time.time()) + '.png').put(event_1_photo, user['idToken'])
-'''
-
